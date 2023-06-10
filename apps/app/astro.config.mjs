c030a0579f1ff358f4e2ff4248aa8fd417b97e2b
ianyimi <anyiminene@gmail.com>
2023-06-10 16:08:03 -0700

    astrop add tailwind & sitemap

--- apps/app/astro.config.mjs
+++ apps/app/astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://tour-genie-turbo.vercel.app',
+  integrations: [
+    react(),
+    tailwind({
+      config: { path: '../../tailwind.config.cjs' },
+    }),
+    sitemap({
+      filer: (page) => !page.path.includes('404'),
+    }),
+  ],
   output: 'server',
   adapter: vercel({
     analytics: true,
